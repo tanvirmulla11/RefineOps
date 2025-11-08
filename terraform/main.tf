79c7a184059ee2b540403739554f57c909ad5d07
tanvirmulla <tanvirmulla73@gmail.com>
2025-11-08 11:22:46 +0530

    FIx Code

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,5 @@
-resource "aws_instance" "k3s_server" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  key_name      = var.key_name
-
-  tags = {
-    Name = var.instance_name
-  }
-
-  security_groups = [aws_security_group.allow_k3s.name]
+provider "aws" {
+  region = "us-east-1"
 }
 
 resource "aws_security_group" "allow_k3s" {
@@ -48,4 +40,25 @@ resource "aws_security_group" "allow_k3s" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+resource "aws_instance" "k3s_server" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  security_groups = [aws_security_group.allow_k3s.name]
+
+  tags = {
+    Name = var.instance_name
+  }
+
+  user_data = <<-EOF
+    #!/bin/bash
+    apt update -y
+    apt install -y docker.io curl git
+    systemctl enable --now docker
+    curl -sfL https://get.k3s.io | sh -
+    docker run -d --name prometheus -p 9090:9090 prom/prometheus
+    docker run -d --name grafana -p 3000:3000 grafana/grafana
+  EOF
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,7 +3,7 @@ variable "instance_type" {
 }
 
 variable "ami_id" {
-  default = "ami-0e306788ff2473ccb"
+  default = "ami-0e306788ff2473ccb" # Ubuntu 22.04 (N. Virginia)
 }
 
 variable "key_name" {
